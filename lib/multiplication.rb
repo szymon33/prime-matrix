8ea0fa95ab1055cf0bef2ce212ad25daba9e869f
szymon33 <miedziejko@gmail.com>
2018-10-17 17:05:05 +0100

    Implement build matrix method

--- lib/multiplication.rb
+++ lib/multiplication.rb
@@ -1,9 +1,14 @@
-class Multiplication
-  FIRST_TEN_PRIME_NUMBERS = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29].freeze
 
+class Multiplication
   def initialize(table_size)
     @table_size = table_size
-    @matrix = valid? ? build_matrix : FIRST_TEN_PRIME_NUMBERS
+    if valid?
+      @vector = vector
+      build_matrix
+    else
+      @table_size = 10
+      @matrix = vector
+    end
   end
 
   private
@@ -21,6 +26,32 @@ class Multiplication
   end
 
   def build_matrix
-    []
+    # address 0,0 is special and is always nil
+    @matrix = Array.new(@table_size + 1) { Array.new(@table_size + 1) }
+    setup_coordinates
+    @vector.each_with_index do |col_val, col|
+      @vector.each_with_index do |row_val, row|
+        @matrix[col + 1][row + 1] = col_val * row_val
+      end
+    end
+    @matrix
+  end
+
+  # vector of prime numbers size of table_size
+  def vector
+    result = []
+    next_candiate = 2
+    while result.size < @table_size
+      (result << next_candiate) if prime?(next_candiate)
+      next_candiate += 1
+    end
+    result
+  end
+
+  def setup_coordinates
+    @matrix[0] = [nil] + @vector
+    @matrix[0].each_with_index do |elem, idx|
+      @matrix[idx][0] = elem
+    end
   end
 end
--- spec/lib/multiplication_spec.rb
+++ spec/lib/multiplication_spec.rb
@@ -1,6 +1,70 @@
 require 'multiplication'
 
 describe Multiplication do
+  describe '.build_matrix' do
+    subject do
+      multiplication = described_class.new(num)
+      multiplication.instance_variable_set('@table_size', num)
+      multiplication.send(:build_matrix)
+    end
+
+    context 'example size 1' do
+      let(:num) { 1 }
+
+      it {
+        is_expected.to eq [[nil, 2],
+                           [2,   4]]
+      }
+    end
+
+    context 'example size 2' do
+      let(:num) { 2 }
+
+      it {
+        is_expected.to eq [[nil, 2, 3],
+                           [2,   4, 6],
+                           [3,   6, 9]]
+      }
+    end
+
+    context 'example size 3' do
+      let(:num) { 3 }
+
+      it {
+        is_expected.to eq [[nil, 2,  3,  5],
+                           [2,   4,  6, 10],
+                           [3,   6,  9, 15],
+                           [5,  10, 15, 25]]
+      }
+    end
+  end
+
+  describe '.vector' do
+    subject do
+      multiplication = described_class.new(num)
+      multiplication.instance_variable_set('@table_size', num)
+      multiplication.send(:vector)
+    end
+
+    context 'with one prime number' do
+      let(:num) { 1 }
+
+      it { is_expected.to eq [2] }
+    end
+
+    context 'with two prime numbers' do
+      let(:num) { 2 }
+
+      it { is_expected.to eq [2, 3] }
+    end
+
+    context 'with 5 prime numbers' do
+      let(:num) { 5 }
+
+      it { is_expected.to eq [2, 3, 5, 7, 11] }
+    end
+  end
+
   describe '.valid?' do
     subject do
       multiplication = described_class.new(num)
