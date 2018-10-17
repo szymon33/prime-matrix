ba6567f16454e98d52c15db714658b4eee70200b
szymon33 <miedziejko@gmail.com>
2018-10-17 13:05:05 +0100

    Add first 10 prime numbers default response and validation method

--- lib/multiplication.rb
+++ lib/multiplication.rb
@@ -1,4 +1,11 @@
 class Multiplication
+  FIRST_TEN_PRIME_NUMBERS = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29].freeze
+
+  def initialize(table_size)
+    @table_size = table_size
+    @matrix = valid? ? build_matrix : FIRST_TEN_PRIME_NUMBERS
+  end
+
   private
 
   def prime?(num)
@@ -7,5 +14,13 @@ class Multiplication
     (2..Math.sqrt(num)).each do |i|
       return false if (num % i).zero?
     end
+  end
+
+  def valid?
+    @table_size&.class == Fixnum
+  end
+
+  def build_matrix
+    []
   end
 end
--- spec/lib/multiplication_spec.rb
+++ spec/lib/multiplication_spec.rb
@@ -1,8 +1,34 @@
 require 'multiplication'
 
 describe Multiplication do
+  describe '.valid?' do
+    subject do
+      multiplication = described_class.new(num)
+      multiplication.instance_variable_set('@table_size', num)
+      multiplication.send(:valid?)
+    end
+
+    context 'is true when table_size is a number' do
+      let(:num) { 5 }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'is false when table_size is nil' do
+      let(:num) { nil }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'is false when table_sie is not a number' do
+      let(:num) { 'La La Land' }
+
+      it { is_expected.to be_falsey }
+    end
+  end
+
   describe '.prime?' do
-    subject { described_class.new.send(:prime?, num) }
+    subject { described_class.new(10).send(:prime?, num) }
 
     context 'for number less or equal to 1' do
       let(:num) { 1 }
@@ -29,7 +55,7 @@ describe Multiplication do
     end
 
     context 'for bigger, not prime number like 10000' do
-      let(:num) { 10000 }
+      let(:num) { 10_000 }
 
       it { is_expected.to be_falsey }
     end
